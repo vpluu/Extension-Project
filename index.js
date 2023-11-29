2a5ed62284e99efee49ee0f222eeb7f5a0caff80
Vian <89276801+vpluu@users.noreply.github.com>
2023-11-29 00:37:28 -0600

    Updated upcoming tasks section

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cancelButton = document.getElementById('cancel-button');
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    const upcomingTasksElement = document.getElementById('upcoming-tasks');
+    const upcomingTasksElement = document.getElementById('scroll-container');
   
     // Retrieve and display upcoming alarms
     getUpcomingTasks(function (upcomingTasks) {
@@ -19,23 +19,66 @@ function getUpcomingTasks(callback) {
 }
 
 function displayUpcomingTasks(alarms) {
-    const upcomingTasksElement = document.getElementById('upcoming-tasks');
+    const upcomingTasksElement = document.getElementById('scroll-container');
   
     if (alarms.length > 0) {
-      const alarmList = document.createElement('ul');
+        // Build a list of upcoming alarms
+        alarms.forEach(function (alarm) {
+          const taskContainer = document.createElement('div');
+          taskContainer.classList.add('task-container');
+    
+          const formattedDate = formatDate(new Date(alarm.scheduledTime));
+          const formattedTime = formatTime(new Date(alarm.scheduledTime));
 
-      alarms.forEach(function (alarm) {
-        const listItem = document.createElement('li');
-        listItem.textContent = `Task at ${new Date(alarm.scheduledTime)}`;
-        alarmList.appendChild(listItem);
-      });
+          const dateTime = document.createElement('div');
+          dateTime.classList.add('date-time');
+
+          const date = document.createElement('div');
+          date.textContent = `${formattedDate}`;
+          date.classList.add('date');
+
+          const time = document.createElement('div');
+          time.textContent = ` ${formattedTime}`;
+          time.classList.add('time');
+
+          const taskName = document.createElement('div');
+          taskName.textContent = `${alarm.name}`;
+          taskName.classList.add('name');
+    
+          const cancelIcon = document.createElement('button');
+          cancelIcon.classList.add('cancel-button');
+          cancelIcon.innerHTML = '❌';
+          cancelIcon.addEventListener('click', function () {
+            // Call a function to handle canceling the alarm
+            cancelTask(alarm.name);
+            // Update the displayed alarms after canceling
+            displayUpcomingTasks(alarms.filter(a => a.name !== alarm.name));
+          });
+    
+          dateTime.appendChild(date);
+          dateTime.appendChild(time);
+          taskContainer.appendChild(dateTime);
+          taskContainer.appendChild(taskName);
+          taskContainer.appendChild(cancelIcon);
+    
+          upcomingTasksElement.appendChild(taskContainer);
+        });
+      } else {
+        // Display a message if there are no upcoming alarms
+        upcomingTasksElement.textContent = 'No upcoming tasks.';
+      }
+  }
+
+  // Format date in MM/DD/YYYY
+function formatDate(date) {
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+    return new Intl.DateTimeFormat('en-US', options).format(date);
+  }
   
-      // Display the list in the specified element
-      upcomingTasksElement.appendChild(alarmList);
-    } else {
-      // Display a message if there are no upcoming alarms
-      upcomingTasksElement.textContent = 'No upcoming tasks.';
-    }
+  // Format time in HH:mm
+  function formatTime(date) {
+    const options = { hour: '2-digit', minute: '2-digit', hour12: false };
+    return new Intl.DateTimeFormat('en-US', options).format(date);
   }
 
 form.addEventListener('submit', (event) => {
